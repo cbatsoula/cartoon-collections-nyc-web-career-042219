8b3bc4f0f199424f5e9fd8e14bfa03c26c0b3aa9
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-09 23:21:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,23 +4,19 @@ def roll_call_dwarves(array)
     puts "#{indexplusone}. #{value}"
   end
 end
-
 def summon_captain_planet(array)
   array.collect do  |word|
     word.capitalize + "!"
   end
 end
-
 def long_planeteer_calls(calls)
   calls.any? do |calls|
     calls.length > 4
 end
 end
-
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
     cheese_types.include?(type)
   end 
 end
-
