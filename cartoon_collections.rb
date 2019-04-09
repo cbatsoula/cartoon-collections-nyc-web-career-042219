8afbd6c819e09be4e44ac50556fdb8485c69f5d0
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-09 23:22:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,22 +1,22 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |value, index|
+def roll_call_dwarves(array)
+  array.each_with_index do |value, index|
     indexplusone = index + 1
     puts "#{indexplusone}. #{value}"
   end
 end
-
-def summon_captain_planet(planeteer_calls)
+def summon_captain_planet(array)
+  array.collect do  |word|
+    word.capitalize + "!"
+  end
 end
-
 def long_planeteer_calls(calls)
   calls.any? do |calls|
     calls.length > 4
 end
 end
-
-#cheese_types = ["cheddar", "gouda", "camembert"]
 def find_the_cheese(array)
-  
-    
-end 
-
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
